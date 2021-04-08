64944191b4f6fe590226b57bd8381ce7de56ecbb
Euctemon <dave@vodickovi.cz>
2021-04-08 16:36:05 +0200

    přidání CO2

--- src/application.c
+++ src/application.c
@@ -4,59 +4,70 @@
 
 #include <application.h>
 
+#define TMP112_PUB_NO_CHANGE_INTERVAL (15 * 60 * 1000)
+#define TMP112_PUB_VALUE_CHANGE 0.2f
+#define TMP112_UPDATE_INTERVAL (2 * 1000)
+
+#define CO2_PUB_NO_CHANGE_INTERVAL (15 * 60 * 1000)
+#define CO2_PUB_VALUE_CHANGE 50.0f
+#define CO2_UPDATE_INTERVAL (1 * 60 * 1000)
+
+
 // LED instance
 twr_led_t led;
 
-// Button instance
-twr_button_t button;
-
-// Thermometer instance
-twr_tmp112_t tmp112;
-uint16_t button_click_count = 0;
-
-// Button event callback
-void button_event_handler(twr_button_t *self, twr_button_event_t event, void *event_param)
-{
-    // Log button event
-    twr_log_info("APP: Button event: %i", event);
-
-    float ppm = 0.0;
-
-
-    if (event == TWR_BUTTON_EVENT_PRESS)
-    {
-        twr_module_co2_measure();
-        twr_module_co2_get_concentration_ppm(&ppm);
-
-        twr_log_debug("CO2 level: %.4fppm", ppm);
-    }
-
-
-    // Check event source
-    if (event == BC_BUTTON_EVENT_CLICK)
-    {
-        // Toggle LED pin state
-        twr_led_set_mode(&led, TWR_LED_MODE_TOGGLE);
-
-         // Publish message on radio
-        button_click_count++;
-        twr_radio_pub_push_button(&button_click_count);
-    }
-}
-
 void tmp112_event_handler(twr_tmp112_t *self, twr_tmp112_event_t event, void *event_param)
 {
-    if (event == TWR_TMP112_EVENT_UPDATE)
+    float value;
+    event_param_t *param = (event_param_t *)event_param;
+
+    if (event != TWR_TMP112_EVENT_UPDATE)
     {
-        float celsius;
-        // Read temperature
-        twr_tmp112_get_temperature_celsius(self, &celsius);
+        return;
+    }
 
-        twr_log_debug("APP: temperature: %.2f °C", celsius);
+    if (twr_tmp112_get_temperature_celsius(self, &value))
+    {
+        if ((fabs(value - param->value) >= TMP112_PUB_VALUE_CHANGE) || (param->next_pub < twr_scheduler_get_spin_tick()))
+        {
+            twr_radio_pub_temperature(param->channel, &value);
+            twr_log_debug("APP: temperature: %.2f °C", value);
 
-        twr_radio_pub_temperature(TWR_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_ALTERNATE, &celsius);
+            param->value = value;
+            param->next_pub = twr_scheduler_get_spin_tick() + TMP112_PUB_NO_CHANGE_INTERVAL;
+
+            //values.temperature = value;
+            twr_scheduler_plan_now(0);
+        }
     }
 }
+
+void co2_event_handler(twr_module_co2_event_t event, void *event_param)
+{
+    event_param_t *param = (event_param_t *) event_param;
+    float value;
+
+    if (event == TWR_MODULE_CO2_EVENT_UPDATE)
+    {
+        if (twr_module_co2_get_concentration_ppm(&value))
+        {
+            if ((fabs(value - param->value) >= CO2_PUB_VALUE_CHANGE) || (param->next_pub < twr_scheduler_get_spin_tick()))
+            {
+                twr_radio_pub_co2(&value);
+                twr_log_debug("APP: CO2: %.0f ppm", value);
+                param->value = value;
+                param->next_pub = twr_scheduler_get_spin_tick() + CO2_PUB_NO_CHANGE_INTERVAL;
+
+                //values.co2_concentation = value;
+                twr_scheduler_plan_now(0);
+            }
+        }
+    }
+}
+
+
+
+
 
 // Application initialization function which is called once after boot
 void application_init(void)
@@ -68,34 +79,21 @@ void application_init(void)
     twr_led_init(&led, TWR_GPIO_LED, false, 0);
     twr_led_pulse(&led, 2000);
 
-    // Initialize button
-    twr_button_init(&button, TWR_GPIO_BUTTON, TWR_GPIO_PULL_DOWN, 0);
-    twr_button_set_event_handler(&button, button_event_handler, NULL);
+    // Temperature
+    static twr_tmp112_t temperature;
+    static event_param_t temperature_event_param = { .next_pub = 0, .channel = TWR_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_ALTERNATE };
+    twr_tmp112_init(&temperature, TWR_I2C_I2C0, 0x49);
+    twr_tmp112_set_event_handler(&temperature, tmp112_event_handler, &temperature_event_param);
+    twr_tmp112_set_update_interval(&temperature, TMP112_UPDATE_INTERVAL);
 
-    // Initialize thermometer on core module
-    twr_tmp112_init(&tmp112, TWR_I2C_I2C0, 0x49);
-    twr_tmp112_set_event_handler(&tmp112, tmp112_event_handler, NULL);
-    twr_tmp112_set_update_interval(&tmp112, 10000);
-
+    // CO2
+    static event_param_t co2_event_param = { .next_pub = 0 };
     twr_module_co2_init();
-
-
-
+    twr_module_co2_set_update_interval(CO2_UPDATE_INTERVAL);
+    twr_module_co2_set_event_handler(co2_event_handler, &co2_event_param);
 
     // Initialize radio
     twr_radio_init(TWR_RADIO_MODE_NODE_SLEEPING);
     // Send radio pairing request
-    twr_radio_pairing_request("skeleton", VERSION);
-}
-
-// Application task function (optional) which is called peridically if scheduled
-void application_task(void)
-{
-    static int counter = 0;
-
-    // Log task run and increment counter
-    twr_log_debug("APP: Task run (count: %d)", ++counter);
-
-    // Plan next run of this task in 1000 ms
-    twr_scheduler_plan_current_from_now(1000);
+    twr_radio_pairing_request("room", VERSION);
 }
