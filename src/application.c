5df8fd322d57199365070366072830f819b86283
Euctemon <dave@vodickovi.cz>
2021-04-14 20:13:11 +0200

    LED limit

--- src/application.c
+++ src/application.c
@@ -12,6 +12,27 @@
 #define CO2_PUB_VALUE_CHANGE 50.0f
 #define CO2_UPDATE_INTERVAL (1 * 60 * 1000)
 
+#define VOC_LP_TAG_PUB_NO_CHANGE_INTERVAL (15 * 60 * 1000)
+#define VOC_LP_TAG_PUB_VALUE_CHANGE 1.0f
+#define VOC_LP_TAG_UPDATE_INTERVAL (5 * 1000)
+
+#define HUMIDITY_TAG_PUB_NO_CHANGE_INTERVAL (15 * 60 * 1000)
+#define HUMIDITY_TAG_PUB_VALUE_CHANGE 5.0f
+#define HUMIDITY_TAG_UPDATE_INTERVAL (2 * 1000)
+
+static struct
+{
+    float_t temperature;
+    float_t humidity;
+    float_t tvoc;
+    float_t pressure;
+    float_t altitude;
+    float_t co2_concentation;
+    float_t battery_voltage;
+    float_t battery_pct;
+
+} values;
+
 
 // LED instance
 twr_led_t led;
@@ -36,7 +57,7 @@ void tmp112_event_handler(twr_tmp112_t *self, twr_tmp112_event_t event, void *ev
             param->value = value;
             param->next_pub = twr_scheduler_get_spin_tick() + TMP112_PUB_NO_CHANGE_INTERVAL;
 
-            //values.temperature = value;
+            values.temperature = value;
             twr_scheduler_plan_now(0);
         }
     }
@@ -58,16 +79,39 @@ void co2_event_handler(twr_module_co2_event_t event, void *event_param)
                 param->value = value;
                 param->next_pub = twr_scheduler_get_spin_tick() + CO2_PUB_NO_CHANGE_INTERVAL;
 
-                //values.co2_concentation = value;
+                values.co2_concentation = value;
                 twr_scheduler_plan_now(0);
             }
         }
     }
 }
 
+void voc_lp_tag_event_handler(twr_tag_voc_lp_t *self, twr_tag_voc_lp_event_t event, void *event_param)
+{
+    event_param_t *param = (event_param_t *)event_param;
 
+    if (event == TWR_TAG_VOC_LP_EVENT_UPDATE)
+    {
+        uint16_t value;
 
+        if (twr_tag_voc_lp_get_tvoc_ppb(self, &value))
+        {
+            if ((fabsf(value - param->value) >= VOC_LP_TAG_PUB_VALUE_CHANGE) || (param->next_pub < twr_scheduler_get_spin_tick()))
+            {
+                param->value = value;
+                param->next_pub = twr_scheduler_get_spin_tick() + VOC_LP_TAG_PUB_NO_CHANGE_INTERVAL;
 
+                int radio_tvoc = value;
+
+                values.tvoc = radio_tvoc;
+
+                twr_radio_pub_int("voc-lp-sensor/0:0/tvoc", &radio_tvoc);
+                twr_log_debug("APP: VOC: %d ppb", radio_tvoc);
+                twr_scheduler_plan_now(0);
+            }
+        }
+    }
+}
 
 // Application initialization function which is called once after boot
 void application_init(void)
@@ -92,8 +136,36 @@ void application_init(void)
     twr_module_co2_set_update_interval(CO2_UPDATE_INTERVAL);
     twr_module_co2_set_event_handler(co2_event_handler, &co2_event_param);
 
+    // VOC-LP
+    static twr_tag_voc_lp_t voc_lp;
+    static event_param_t voc_lp_event_param = { .next_pub = 0 };
+    twr_tag_voc_lp_init(&voc_lp, TWR_I2C_I2C0);
+    twr_tag_voc_lp_set_event_handler(&voc_lp, voc_lp_tag_event_handler, &voc_lp_event_param);
+    twr_tag_voc_lp_set_update_interval(&voc_lp, VOC_LP_TAG_UPDATE_INTERVAL);
+
+
     // Initialize radio
     twr_radio_init(TWR_RADIO_MODE_NODE_SLEEPING);
     // Send radio pairing request
     twr_radio_pairing_request("room", VERSION);
+}
+void application_task(void){
+    bool limit = false;
+    if (values.temperature>25.0){
+        limit = true;
+    }
+    if (values.co2_concentation>1000.0){
+        limit = true;
+    }
+    if (values.tvoc>60.0){
+        limit = true;
+    }
+
+    if (limit)
+        twr_led_set_mode(&led,TWR_LED_MODE_ON);
+    else
+        twr_led_set_mode(&led,TWR_LED_MODE_OFF);
+
+
+    twr_scheduler_plan_current_relative(1000);
 }
